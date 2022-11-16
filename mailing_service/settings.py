5bcbaa8ce556944c79de621d238f82a5ca463a59
Emir Saparov <emiracle1435@gmail.com>
2022-11-17 00:18:17 +0300

    settings changes

--- mailing_service/settings.py
+++ mailing_service/settings.py
@@ -44,8 +44,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'emiracle1435@gmail.com'
-EMAIL_HOST_PASSWORD = 'dddwldpoezqynfpp'
+EMAIL_HOST_USER = 'sender host email'
+EMAIL_HOST_PASSWORD = 'host password'
 EMAIL_USE_SSL = False
 
 MIDDLEWARE = [
